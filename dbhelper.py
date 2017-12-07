89b0a09a5134156dbdf46638e9f78d3c875f6883
stefan <stefan@stefan.com>
2017-12-08 00:29:36 +0100

    DB connect

--- dbhelper.py
+++ dbhelper.py
@@ -22,10 +22,9 @@ class DBHelper:
     def add_input(self, data):
         connection = self.connect()
         try:
-            # The following introduces a deliberate security flaw. See section on SQL injection below
-            query = "INSERT INTO crimes (description) VALUES ('{}');".format(data)
+            query = "INSERT INTO crimes (description) VALUES (%s);"
             with connection.cursor() as cursor:
-                cursor.execute(query)
+                cursor.execute(query, data)
                 connection.commit()
         finally:
             connection.close()
@@ -37,5 +36,17 @@ class DBHelper:
             with connection.cursor() as cursor:
                 cursor.execute(query)
                 connection.commit()
+        finally:
+            connection.close()
+
+    def add_crime(self, category, date, latitude, longitude, description):
+        connection = self.connect()
+        try:
+            query = "INSERT INTO crimes (category, date, latitude, longitude, description) VALUES (%s, %s, %s, %s, %s)"
+            with connection.cursor() as cursor:
+                cursor.execute(query, (category, date, latitude, longitude, description))
+                connection.commit()
+        except Exception as e:
+            print(e)
         finally:
             connection.close()
