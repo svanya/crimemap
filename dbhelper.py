df5148aedce99355fe748fea74fbbcce007feb53
stefan <stefan@stefan.com>
2017-12-08 13:36:01 +0100

    bug 3

--- dbhelper.py
+++ dbhelper.py
@@ -60,14 +60,14 @@ class DBHelper:
                 cursor.execute(query)
             named_crimes = []
             for crime in cursor:
-            named_crime = {
-                'latitude': crime[0],
-                'longitude': crime[1],
-                'date': datetime.datetime.strftime(crime[2], '%Y-%m-%d'),
-                'category': crime[3],
-                'description': crime[4]
-            }
-            named_crimes.append(named_crime)
+                named_crime = {
+                    'latitude': crime[0],
+                    'longitude': crime[1],
+                    'date': datetime.datetime.strftime(crime[2], '%Y-%m-%d'),
+                    'category': crime[3],
+                    'description': crime[4]
+                }
+                named_crimes.append(named_crime)
             return named_crimes
         finally:
             connection.close()
