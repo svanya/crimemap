3f3c713feb6507d7758f1c2f30226be41f8f52b9
stefan <stefan@stefan.com>
2017-12-10 02:10:14 +0100

    Final v1

--- crimemap.py
+++ crimemap.py
@@ -2,6 +2,9 @@ from flask import Flask
 from flask import render_template
 from flask import request
 import json
+import dateparser
+import datetime
+import string
 import dbconfig
 if dbconfig.test:
     from mockdbhelper import MockDBHelper as DBHelper
@@ -10,42 +13,46 @@ else:
 
 app = Flask(__name__)
 DB = DBHelper()
+categories = ['přepadení', 'vloupání']
+
+
+def format_date(userdate):
+    date = dateparser.parse(userdate)
+    try:
+        return datetime.datetime.strftime(date, "%Y-%m-%d")
+    except TypeError:
+        return None
+
+
+def sanitize_string(userinput):
+    whitelist = string.ascii_letters + string.digits + " !?$.,;:-'()&"
+    return filter(lambda x: x in whitelist, userinput)
 
 
 @app.route("/")
-def home():
+def home(error_message=None):
     crimes = DB.get_all_crimes()
     crimes = json.dumps(crimes)
-    return render_template("home.html", crimes=crimes)
+    return render_template("home.html", crimes=crimes, categories=categories, error_message=error_message)
 
-
-@app.route("/add", methods=["POST"])
-def add():
-    try:
-        data = request.form.get("userinput")
-        DB.add_input(data)
-    except Exception as e:
-        print(e)
-    return home()
-
-
-@app.route("/clear")
-def clear():
-    try:
-        DB.clear_all()
-    except Exception as e:
-        print(e)
-    return home()
 
 @app.route("/submitcrime", methods=['POST'])
 def submitcrime():
     category = request.form.get("category")
-    date = request.form.get("date")
-    latitude = float(request.form.get("latitude"))
-    longitude = float(request.form.get("longitude"))
-    description = request.form.get("description")
+    if category not in categories:
+        return home()
+    date = format_date(request.form.get("date"))
+    if not date:
+        return home("Invalid date. Please use yyyy-mm-dd format")
+    try:
+        latitude = float(request.form.get("latitude"))
+        longitude = float(request.form.get("longitude"))
+    except ValueError:
+        return home()
+    description = sanitize_string(request.form.get("description"))
     DB.add_crime(category, date, latitude, longitude, description)
     return home()
+
 
 if __name__ == '__main__':
     app.run(host='localhost', port=5000, debug=True)
--- dbhelper.py
+++ dbhelper.py
@@ -2,6 +2,7 @@ import pymysql
 import dbconfig
 import datetime
 
+
 class DBHelper:
 
     def connect(self, database="crimemap"):
@@ -9,36 +10,6 @@ class DBHelper:
                                user=dbconfig.db_user,
                                passwd=dbconfig.db_password,
                                db=database)
-
-    def get_all_inputs(self):
-        connection = self.connect()
-        try:
-            query = "SELECT description FROM crimes;"
-            with connection.cursor() as cursor:
-                cursor.execute(query)
-            return cursor.fetchall()
-        finally:
-            connection.close()
-
-    def add_input(self, data):
-        connection = self.connect()
-        try:
-            query = "INSERT INTO crimes (description) VALUES (%s);"
-            with connection.cursor() as cursor:
-                cursor.execute(query, data)
-                connection.commit()
-        finally:
-            connection.close()
-
-    def clear_all(self):
-        connection = self.connect()
-        try:
-            query = "DELETE FROM crimes;"
-            with connection.cursor() as cursor:
-                cursor.execute(query)
-                connection.commit()
-        finally:
-            connection.close()
 
     def add_crime(self, category, date, latitude, longitude, description):
         connection = self.connect()
